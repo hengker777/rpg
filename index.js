31f8a37ee171c15260f346b1387e95774b717330
hengker777 <putraalvino759@gmail.com>
2022-10-29 14:53:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('KingOfBear', {
+say('CLOWNSTORE', {
   font: 'chrome',
   align: 'center',
   colors: ['red', 'magenta']
 })
-say(`⧻ YukiBot2022 By @KingOfBear`, { 
+say(`⧻ CLOWNBOT By CLOWNSTORE`, { 
   font: 'console',
   align: 'center',
   colors: ['red', 'magenta']
